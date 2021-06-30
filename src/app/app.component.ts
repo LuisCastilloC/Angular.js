fb17e1462ce03ec9440cf43c9a26fabeb51a6b20
Luis Fernando Castillo Cordova <17470062@campeche.tecnm.mx>
2021-06-30 18:43:25 -0500

    Se agrego el componente Dashboard

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,22 +1,7 @@
-// Importa dependencias
-import { StocksService, StockInterface } from './services/stocks.service'; 
-
 import { Component } from '@angular/core';
-import { from } from 'rxjs';
-
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+ selector: 'app-root',
+ templateUrl: './app.component.html',
+ styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'stocks';
-  // Declara una propiedad como un arreglo de acciones 
- stocks: Array<StockInterface> = [];
- constructor(service: StocksService) {
- service.load(['AAPL']).subscribe(stocks => {
- // Una vez que se cargan los datos los almacena en la propiedad stocks.
- this.stocks = stocks;
- });
- }
-}
+export class AppComponent { }
