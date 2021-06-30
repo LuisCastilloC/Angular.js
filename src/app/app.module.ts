45820786af351bba967304cf705911296365ca59
Luis Fernando Castillo Cordova <17470062@campeche.tecnm.mx>
2021-06-30 15:23:44 -0500

    Se agregó el servicio StocksService

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { StocksService } from './services/stocks.service';
+import { HttpClientModule } from '@angular/common/http';
 
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -8,9 +10,10 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [StocksService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
